b66d3639489b380cd2fac4ea9180a43244fe96fb
Chaudhry-Ilyas <66716403+Chaudhry-Ilyas@users.noreply.github.com>
2023-11-23 22:45:14 +0500

    add api-frontends

--- frontend/src/components/addEmployee/Add.tsx
+++ frontend/src/components/addEmployee/Add.tsx
@@ -1,4 +1,4 @@
-import { GridColDef } from "@mui/x-data-grid";
+/*import { GridColDef } from "@mui/x-data-grid";
 import "./add.scss";
 // import { useMutation, useQueryClient } from "@tanstack/react-query";
 
@@ -9,7 +9,6 @@ type Props = {
 };
 
 const Add = (props: Props) => {
-
   // TEST THE API
 
   // const queryClient = useQueryClient();
@@ -44,7 +43,7 @@ const Add = (props: Props) => {
 
     //add new item
     // mutation.mutate();
-    props.setOpen(false)
+    props.setOpen(false);
   };
   return (
     <div className="add">
@@ -63,6 +62,84 @@ const Add = (props: Props) => {
               </div>
             ))}
           <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
+*/
+import React, { useState } from "react";
+import { GridColDef } from "@mui/x-data-grid";
+import "./add.scss";
+
+type Props = {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Add: React.FC<Props> = (props) => {
+  const [formData, setFormData] = useState<{ [key: string]: string }>({});
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3006/api/employee", {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Handle success, e.g., close the modal or show a success message
+        props.setOpen(false);
+      } else {
+        // Handle errors, log or show a message to the user
+        console.error("Failed to add item");
+      }
+    } catch (error) {
+      // Handle network errors
+      console.error("Network error:", error);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          X
+        </span>
+        <h1>Add new {props.slug}</h1>
+        <form onSubmit={handleSubmit}>
+          {props.columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map((column) => (
+              <div className="item" key={column.field}>
+                <label>{column.headerName}</label>
+                <input
+                  type={column.type || "text"}
+                  placeholder={column.field}
+                  name={column.field}
+                  value={formData[column.field] || ""}
+                  onChange={handleChange}
+                />
+              </div>
+            ))}
+          <button type="submit">Send</button>
         </form>
       </div>
     </div>
