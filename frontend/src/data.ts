8c80d277b3d26fcb10a7d24b31c9a3f43928f0a9
Chaudhry-Ilyas <66716403+Chaudhry-Ilyas@users.noreply.github.com>
2023-11-27 17:41:36 +0500

    homepage icons

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -90,102 +90,102 @@ export const menu = [
     ],
   },
   //end for shop owners
-  {
-    id: 3,
-    title: "lists",
-    listItems: [
-      {
-        id: 1,
-        title: "Users",
-        url: "/users",
-        icon: "user.svg",
-      },
-      {
-        id: 2,
-        title: "Products",
-        url: "/products",
-        icon: "product.svg",
-      },
-      {
-        id: 3,
-        title: "Orders",
-        url: "/orders",
-        icon: "order.svg",
-      },
-      {
-        id: 4,
-        title: "Posts",
-        url: "/posts",
-        icon: "post2.svg",
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "general",
-    listItems: [
-      {
-        id: 1,
-        title: "Elements",
-        url: "/",
-        icon: "element.svg",
-      },
-      {
-        id: 2,
-        title: "Notes",
-        url: "/",
-        icon: "note.svg",
-      },
-      {
-        id: 3,
-        title: "Forms",
-        url: "/",
-        icon: "form.svg",
-      },
-      {
-        id: 4,
-        title: "Calendar",
-        url: "/",
-        icon: "calendar.svg",
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: "Maintenance",
-    listItems: [
-      {
-        id: 1,
-        title: "Settings",
-        url: "/",
-        icon: "setting.svg",
-      },
-      {
-        id: 2,
-        title: "Backups",
-        url: "/",
-        icon: "backup.svg",
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: "analytics",
-    listItems: [
-      {
-        id: 1,
-        title: "Charts",
-        url: "/",
-        icon: "chart.svg",
-      },
-      {
-        id: 2,
-        title: "Logs",
-        url: "/",
-        icon: "log.svg",
-      },
-    ],
-  },
+  // {
+  //   id: 3,
+  //   title: "lists",
+  //   listItems: [
+  //     {
+  //       id: 1,
+  //       title: "Users",
+  //       url: "/users",
+  //       icon: "user.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "Products",
+  //       url: "/products",
+  //       icon: "product.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       title: "Orders",
+  //       url: "/orders",
+  //       icon: "order.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       title: "Posts",
+  //       url: "/posts",
+  //       icon: "post2.svg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 4,
+  //   title: "general",
+  //   listItems: [
+  //     {
+  //       id: 1,
+  //       title: "Elements",
+  //       url: "/",
+  //       icon: "element.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "Notes",
+  //       url: "/",
+  //       icon: "note.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       title: "Forms",
+  //       url: "/",
+  //       icon: "form.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       title: "Calendar",
+  //       url: "/",
+  //       icon: "calendar.svg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 5,
+  //   title: "Maintenance",
+  //   listItems: [
+  //     {
+  //       id: 1,
+  //       title: "Settings",
+  //       url: "/",
+  //       icon: "setting.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "Backups",
+  //       url: "/",
+  //       icon: "backup.svg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 6,
+  //   title: "analytics",
+  //   listItems: [
+  //     {
+  //       id: 1,
+  //       title: "Charts",
+  //       url: "/",
+  //       icon: "chart.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "Logs",
+  //       url: "/",
+  //       icon: "log.svg",
+  //     },
+  //   ],
+  // },
 ];
 
 export const topDealUsers = [
